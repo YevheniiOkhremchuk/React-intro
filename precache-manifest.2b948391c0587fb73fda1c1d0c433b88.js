d1b53a3a33c2eda1fee410ae1d898282acb89b3a
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-10 17:38:01 +0300

    Updates

---
+++ precache-manifest.2b948391c0587fb73fda1c1d0c433b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3127a688233f83c119857e958c4ebc33",
+    "url": "/React-intro/index.html"
+  },
+  {
+    "revision": "13ba8e0ae6bff25a6282",
+    "url": "/React-intro/static/css/main.3cabff21.chunk.css"
+  },
+  {
+    "revision": "9831626d10406a4094ce",
+    "url": "/React-intro/static/js/2.26badbf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-intro/static/js/2.26badbf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ba8e0ae6bff25a6282",
+    "url": "/React-intro/static/js/main.aeccd42e.chunk.js"
+  },
+  {
+    "revision": "d375deea939831ab92fe",
+    "url": "/React-intro/static/js/runtime-main.4235fc78.js"
+  }
+]);
